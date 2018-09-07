5e1a9edca907a47f1c535d595f0b8377bb2687ea
dinesh661996 <dinesh.kksm@gmail.com>
2018-09-07 17:04:08 +0530

    This is test page

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace auth.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public static object Microsoft { get; internal set; }
+        public static object IdentityUser { get; internal set; }
+    }
+}
---
+++ Models/UserRoles.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace UserRoles
+{
+    public class UserRoles{
+
+        public int UserId { get; set; }
+
+        public int UserName { get; set; }
+
+        public int Email { get; set; }
+
+        public int Role { get; set; }
+
+    }
+}
